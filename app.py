71275fd73ca4165c84cb999efe3bde0f01c16359
sparklynjewel <55154161+sparklynjewel@users.noreply.github.com>
2020-03-27 00:08:18 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+from math import pi
+radius = float(input("input the radius of the circle : "))
+print ("for a circle with " + str(radius) + " of radius ")
+area = ( pi*radius*radius)
+print ("the area is : " + str(area))
